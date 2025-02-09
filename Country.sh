7c70d4ac877cc3bf13f05ad308dc664091042514
SPEED <rqmtw4058@gmail.com>
2025-02-09 14:41:24 +0200

    Update Country.sh

--- Country.sh
+++ Country.sh
@@ -67,7 +67,7 @@ country_vps_flag() {
         "FO") echo "Faroe Islands 🇫🇴";;
         "FJ") echo "Fiji 🇫🇯";;
         "FI") echo "Finland 🇫🇮";;
-        "FR") echo "France 🇫🇷";;
+        "FR") echo "France speed 🇫🇷";;
         "GA") echo "Gabon 🇬🇦";;
         "GM") echo "Gambia 🇬🇲";;
         "GE") echo "Georgia 🇬🇪";;
