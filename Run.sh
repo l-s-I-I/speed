1f62ed9c282b6322bd6b01b790653821ab736c42
SPEED <rqmtw4058@gmail.com>
2025-04-05 18:52:06 +0200

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -66,11 +66,11 @@ if [ -f /etc/os-release ]; then
     esac
 fi
 
-bash <(curl -s https://raw.githubusercontent.com/l-s-I-I/speed/refs/heads/main/neofetch)
 
 echo -e "\033[1;32m##########################################################################\033[0m"
 echo -e "\033[1;31m#         This script support port 443 on vps one line Command !         #\033[0m"
 echo -e "\033[1;32m##########################################################################\033[0m"
+bash <(curl -s https://raw.githubusercontent.com/l-s-I-I/speed/refs/heads/main/neofetch)
 echo "--------------------------------------------------------------------------"
 echo -e "\033[1;32m                               Start √ \033[0m"
 echo "--------------------------------------------------------------------------"
